23e586d4c3c97b1a4938d8c6001bfaaa5ab72108
Akriti Sneh <91659120+akritisneh@users.noreply.github.com>
2024-11-26 18:43:56 +0530

    init

---
+++ rbac-backend/data/mockPermissions.js
@@ -0,0 +1,20 @@
+const mockPermissions = [
+    {
+      id: 1,
+      module: "IT Systems",
+      actions: ["Manage Users", "Configure Servers", "Monitor Networks"],
+    },
+    {
+      id: 2,
+      module: "HR Systems",
+      actions: ["View Employee Records", "Edit Payroll"],
+    },
+    {
+      id: 3,
+      module: "Finance Systems",
+      actions: ["View Reports", "Edit Budgets"],
+    },
+  ];
+  
+  module.exports = mockPermissions;
+  
