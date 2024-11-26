1110542e92eb4a8b86cae8d0241db132f1f34d0a
Akriti Sneh <91659120+akritisneh@users.noreply.github.com>
2024-11-26 18:34:11 +0530

    init

---
+++ rbac-ui/src/data/mockRoles.js
@@ -0,0 +1,9 @@
+const mockRoles = [
+    { id: 1, name: "IT Admin", description: "Full control of IT systems and infrastructure." },
+    { id: 2, name: "HR Manager", description: "Access to employee data and payroll systems." },
+    { id: 3, name: "Finance Analyst", description: "View financial reports and budgets." },
+    { id: 4, name: "Employee", description: "Basic access to self-service portals." },
+  ];
+  
+  export default mockRoles;
+  
